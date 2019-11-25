ae70365e00c73175d6e4f10e5755bc2dd510ea60
lhousaine <lhoussaineouarhou@gmail.com>
2019-11-25 09:23:20 +0100

    initialization of the project

---
+++ src/main/java/com/remote/united_shop/UnitedShopApplication.java
@@ -0,0 +1,13 @@
+package com.remote.united_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitedShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitedShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/remote/united_shop/UnitedShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remote.united_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitedShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
